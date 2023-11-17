e6302febdc7681f2998ad6c61ab2f4e2a81c8dee
Lee-JaeWon <jawwoni@naver.com>
2023-11-17 19:01:02 +0900

    [Add]:add Docker

---
+++ docker/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+ 
+set -e
+
+# echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
+# echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+
+echo "alias cw='cd ~/workspace'" >> ~/.bashrc
+echo "alias cs='cd ~/workspace/src'" >> ~/.bashrc
+# echo "alias cm='cd ~/catkin_ws && catkin_make'" >> ~/.bashrc
+echo "alias sb='source ~/.bashrc'" >> ~/.bashrc
+
+cd /root/workspace/src
+
+pip install .
+pip install -r requirements.txt
+
+echo "============== LocNDF Docker Env Ready================"
+
+cd /root/workspace
+
+exec "$@"
